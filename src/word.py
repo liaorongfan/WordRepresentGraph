b964eaaf86b578dc650971e9259cbc7bab90fbcc
liaorongfan <15670381505@163.com>
2023-01-23 03:46:19 +0800

    update

---
+++ src/word.py
@@ -0,0 +1,22 @@
+
+
+class WordBase:
+
+    def __init__(self):
+        """
+
+        """
+        self.meaning = ""
+        self.synonym = ""
+        self.label = ""
+        self.phrase = ""
+        self.collocation = ""
+        self.chunk = ""
+        self.sentence = ""
+
+    def __str__(self):
+        pass
+
+
+if __name__ == "__main__":
+    print("word present graph")
